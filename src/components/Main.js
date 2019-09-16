ecffef8e39e33e83ce3ac9abc255d6b0248c1dbe
alfredo <you@example.com>
2019-09-16 09:45:55 -0700

    updat files

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Main() {
+  return (
+
+    <main>
+      <h1>This is from main component</h1>
+
+      <article>
+        <h2>This is the first passage</h2>
+        <p>Welcome to my first react application</p>
+      </article>
+    </main>
+    
+  )
+}
+
+export default Main;
